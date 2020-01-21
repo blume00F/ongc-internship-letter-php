759ca314b67a60c4a59cf79abbbf1d2ce4c30238
Inj3ctoRRR <coolabhishekjack@gmail.com>
2020-01-21 20:34:37 +0530

    final changes

--- database/fillform.sql
+++ database/fillform.sql
@@ -49,14 +49,11 @@ CREATE TABLE `registration` (
 
 INSERT INTO `registration` (`id`, `name`, `date`, `address`, `phone`, `email`, `fname`, `mname`, `institute`, `course`, `marks_sem`, `marks_12th`) VALUES
 (2, 'ak', 13091999, '271 new street', 123, 'sandlamp8909@gmail.com', 'KK', 'LD', 'DIT', 'Btech', 65.00, 89.90),
-(5, 'Amar Kumar', 12, '271, Indira Colony Chukkuwala, Dehradun', 123456789, 'abc@gmail.com', 'Mr. Atmaram Kumar', 'xyz', 'St Agnes ', 'Inter ', 75.00, 82.00),
-(6, 'Abhishek Kumar', 13091999, '271 Indira Colony Chukkuwala', 9675167144, 'abhishekar1399@gmail.com', 'Kailash Kumar', 'Laxmi Devi', 'DIT University', 'BTech CSE', 78.20, 89.00),
+(5, 'Amar Kumar', 12, '271 M Street, Dehradun', 123456789, 'abc@gmail.com', 'Mr. Atmaram Kumar', 'xyz', 'St Agnes ', 'Inter ', 75.00, 82.00),
+(6, 'Abhishek Kumar', 13091999, '271 MR Road', 9675167174, 'abhishekar1399@gmail.com', 'KK', 'LD', 'DIT University', 'BTech CSE', 78.20, 89.00),
 (21, 'yash goyal', 13091998, '271 heera colony', 9999988888, 'ok@gmail.com', 'mahesh goyal', 'rita goyal', 'skm', 'btech', 65.70, 82.90),
-(22, 'Babita Kumari', 26081997, '271 indira colony chukkwala', 8410782063, 'getbabita.26@gmail.com', 'KKP', 'LD', 'gehu', 'cse', 79.00, 72.50),
 (23, 'Sarthak Bhatia', 26091999, '271 machi bazar', 9997146310, 'bhatiasarthak0@gmail.com', 'Surendra Bhatia', 'K Bhatia', 'DIT ', 'cse', 78.00, 89.00),
-(24, 'Shivam Rawat', 26051999, 'IT Park', 899999989, 'faltushiv9@gmail.com', 'SS', 'BB', 'DIT University', 'cse', 80.00, 96.00),
-(25, 'Harshit Upadhyay', 15121999, '9 Patel Nagar', 9411112417, 'Harshit.Upadhyay3099@gmail.com', 'KU', 'MU', 'DIT', 'cse', 84.00, 72.00),
-(26, 'Harshit Singhal', 10041999, '89 chukkuwala dehradun', 7895647447, 'harshitsinghal0410@gmail.com', 'HS', 'MS', 'DIT', 'cse', 92.00, 96.88);
+
 
 --
 -- Indexes for dumped tables
